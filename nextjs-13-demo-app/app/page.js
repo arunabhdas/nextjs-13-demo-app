992c872df3892acca99a06e1bb9087b62078a23b
Arunabh Das <arunabhdas@gmail.com>
2022-10-28 02:55:28 -0400

    Added root navigation routes

--- nextjs-13-demo-app/app/page.js
+++ nextjs-13-demo-app/app/page.js
@@ -6,7 +6,13 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="engines/">Next</a>
+          <a href="engines/">Next</a> 
+          <br/>
+          <a href="engines/">Cafeteria</a> 
+          <br/>
+          <a href="weapons/">Weapons Room</a>
+          <br/>
+          <a href="engines/">Engines Room</a>
         </h1>
 
       </main>
